73539ab38cc69bba730fe7f2e30e6ec9048fe01c
Roman Brito <romanbrito1@gmail.com>
2017-12-21 23:09:22 -0600

    saving image to mongodb

--- server/api/asset/image/imageRoutes.js
+++ server/api/asset/image/imageRoutes.js
@@ -1,14 +1,29 @@
 import {Router} from 'express';
 import multer from 'multer';
+import fs from 'fs';
 import * as controller from "./imageController";
+import Image from './imageModel';
 
 const router = Router();
 
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname)
+  }
+});
+const upload = multer({storage});
 
 router.post('/', upload.single('file-to-upload'), (req, res) => {
+  const newImage = new Image();
+  newImage.img.data = fs.readFileSync('uploads/file-to-upload');
+  newImage.img.contentType = 'image/jpeg';
+  newImage.save();
+
   console.log(req.file);
-  res.json('hello');
+  res.json({'message': 'File uploaded successfully'});
 });
 
 
